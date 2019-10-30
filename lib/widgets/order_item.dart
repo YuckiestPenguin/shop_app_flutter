724ea2903a2f02b919006e7715af520525c82477
Vico Melo <vinicomelo@gmail.com>
2019-10-30 13:16:41 -0400

    converted to stateful widget

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import '../providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,9 +19,9 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('MM/dd/yyyy hh:mm').format(order.dateTime),
+              DateFormat('MM/dd/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing:
                 IconButton(icon: Icon(Icons.expand_more), onPressed: () {}),
