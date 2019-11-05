e917994e2cc5c581da1008607132a0890714dd44
Vico Melo <vinicomelo@gmail.com>
2019-11-04 20:49:04 -0500

    added Manage Orders section

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './screens/product_detail_screen.dart';
 import 'providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
-            OrdersScreen.routeName: (ctx) => OrdersScreen()
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen()
           }),
     );
   }
