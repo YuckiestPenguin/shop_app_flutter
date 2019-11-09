858649bb4b2eeada951025d1bfd1b69f5c009992
Vico Melo <vinicomelo@gmail.com>
2019-11-08 22:19:40 -0500

    used future for post method

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -65,7 +65,8 @@ class Products with ChangeNotifier {
 
   void addProduct(Product product) {
     const url = 'https://flutter-shop-app-3e8c3.firebaseio.com/products.json';
-    http.post(
+    http
+        .post(
       url,
       body: json.encode({
         'title': product.title,
@@ -74,16 +75,19 @@ class Products with ChangeNotifier {
         'imageUrl': product.imageUrl,
         'isFavorite': product.isFavorite,
       }),
-    );
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      price: product.price,
-      description: product.description,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    )
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
