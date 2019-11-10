625c0a66c3058a5dc554ec007bbd9c5e04c99aae
Vico Melo <vinicomelo@gmail.com>
2019-11-10 18:49:07 -0500

    reversed orders list so newest is on top

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -45,7 +45,7 @@ class Orders with ChangeNotifier {
               .toList(),
           dateTime: DateTime.parse(orderData['dateTime'])));
     });
-    _orders = loadedOrders;
+    _orders = loadedOrders.reversed.toList();
     notifyListeners();
     print(json.decode(response.body));
   }
