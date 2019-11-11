234f8ca71bd9bc22940012c75e865def0b5dedd3
Vico Melo <vinicomelo@gmail.com>
2019-11-10 20:30:35 -0500

    added auth card

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/auth-screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,13 +29,14 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.blue,
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato'),
-          home: ProductsOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
             UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-            EditProductScreen.routeName: (ctx) => EditProductScreen()
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+            AuthScreen.routeName: (ctx) => AuthScreen()
           }),
     );
   }
